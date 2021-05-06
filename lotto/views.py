2aca14b2b6f84410b6ee7e3df7ad8304f53eb264
wjdwogk95 <jeha95@naver.com>
2021-05-06 10:02:18 +0900

    First commit

---
+++ lotto/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import GuessNumbers
+from .forms import PostForm
+
+
+# Create your views here.
+def index(request):
+
+    lottos = GuessNumbers.objects.all()
+
+    return render(request, 'lotto/default.html', {'lottos' : lottos})
+
+
+def post(request):
+
+    if request.method == 'POST':
+
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+
+            lotto = form.save(commit=False)
+            lotto.generate()
+
+            return redirect('index')
+
+    else :
+        form = PostForm()
+        return render(request, 'lotto/form.html', {'form' : form})
+
+
+def detail(request, lottokey):
+
+    lotto = GuessNumbers.objects.get(pk=lottokey)
+    
+    return render(request, 'lotto/detail.html', {'lotto' : lotto})
